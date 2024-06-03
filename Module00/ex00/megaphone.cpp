5c558f7928560317ae351e30624bf28167e97068
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-03 18:32:18 -0300

    Update megaphone.cpp

--- Module00/ex00/megaphone.cpp
+++ Module00/ex00/megaphone.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <string>
 
-int	main(int argC, char *argV[])
-{
-	if (argC == 1)
-	{
+int	main(int argC, char *argV[]) {
+	if (argC == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	for (int i = 1; i < argC; i++)
-	{
+	for (int i = 1; i < argC; i++) {
 		std::string str = argV[i];
 		for (size_t j = 0; j < str.length(); j++)
 			std::cout << (char)std::toupper(str[j]);
