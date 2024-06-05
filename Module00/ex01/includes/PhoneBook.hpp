5c33349ecc182043e85aae9a93d6d90cb9f98019
Arthur <tutusobreira@gmail.com>
2024-06-04 21:18:54 -0300

    Update PhoneBook.hpp

--- Module00/ex01/includes/PhoneBook.hpp
+++ Module00/ex01/includes/PhoneBook.hpp
@@ -3,9 +3,19 @@
 
 #include "Contact.hpp"
 
-// class PhoneBook {
-// 	private:
+class PhoneBook {
+	private:
+		Contact	_contacts[8];
+		static int	_contactIndex;
 
-// };
+	public:
+		/* Constructor Method */
+		PhoneBook();
+
+		/* Destructor Method */
+		~PhoneBook();
+
+		
+};
 
 #endif
