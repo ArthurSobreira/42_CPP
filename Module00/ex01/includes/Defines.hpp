7ebc8fa40c8c5998202fb55fa7bfdc289bd60d2d
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-05 11:16:44 -0300

    Update Defines.hpp

--- Module00/ex01/includes/Defines.hpp
+++ Module00/ex01/includes/Defines.hpp
@@ -6,6 +6,7 @@
 
 /* Regular Colors */
 # define BLACK "\033[30m"
+# define GRAY "\033[90m"
 # define RED "\033[31m"
 # define GREEN "\033[32m"
 # define YELLOW "\033[33m"
@@ -16,6 +17,7 @@
 
 /* Bold Colors */
 # define BOLD_BLACK "\033[1;30m"
+# define BOLD_GRAY "\033[1;90m"
 # define BOLD_RED "\033[1;31m"
 # define BOLD_GREEN "\033[1;32m"
 # define BOLD_YELLOW "\033[1;33m"
@@ -26,6 +28,7 @@
 
 /* Underline Colors */
 # define UNDER_BLACK "\033[4;30m"
+# define UNDER_GRAY "\033[4;90m"
 # define UNDER_RED "\033[4;31m"
 # define UNDER_GREEN "\033[4;32m"
 # define UNDER_YELLOW "\033[4;33m"
@@ -33,5 +36,16 @@
 # define UNDER_MAGENTA "\033[4;35m"
 # define UNDER_CYAN "\033[4;36m"
 # define UNDER_WHITE "\033[4;37m"
+
+/* Utils */
+# define MAX_CONTACTS 8
+# define HEADER_SIZE 43
+# define WELCOME_MESSAGE BOLD_GREEN \
+"  Welcome to the PhoneBook! (type 'help')" RESET
+# define HELP_MESSAGE BOLD_WHITE \
+"[ADD]    -  Add a new contact\n\
+[SEARCH] -  Search for a contact\n\
+[EXIT]   -  Exit the PhoneBook" RESET
+# define INPUT_MESSAGE BOLD_GRAY "PhoneBook> " RESET
 
 #endif
