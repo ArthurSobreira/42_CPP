35529394230e12a139dd45a920af756a495c41a5
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-06 03:14:45 -0300

    Update Contact.hpp

--- Module00/ex01/includes/Contact.hpp
+++ Module00/ex01/includes/Contact.hpp
@@ -20,23 +20,23 @@ class Contact {
 
 		/* Getter and Setter _firstName */
 		std::string	getFirstName( void ) const;
-		void		setFirstName(std::string firstName);
+		void		setFirstName( std::string firstName );
 
 		/* Getter and Setter _lastName */
 		std::string	getLastName( void ) const;
-		void		setLastName(std::string lastName);
+		void		setLastName( std::string lastName );
 
 		/* Getter and Setter _nickname */
 		std::string	getNickname( void ) const;
-		void		setNickname(std::string nickname);
+		void		setNickname( std::string nickname );
 
 		/* Getter and Setter _phoneNumber */
 		std::string	getPhoneNumber( void ) const;
-		void		setPhoneNumber(std::string phoneNumber);
+		void		setPhoneNumber( std::string phoneNumber );
 
 		/* Getter and Setter _darkestSecret */
 		std::string	getDarkestSecret( void ) const;
-		void		setDarkestSecret(std::string darkestSecret);
+		void		setDarkestSecret( std::string darkestSecret );
 };
 
 #endif
