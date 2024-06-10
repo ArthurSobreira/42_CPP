25fa48e7d596a7333f5441b0c33592ba7b502c4d
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 18:22:19 -0300

    Update Harl.hpp

---
+++ Module01/ex06/includes/Harl.hpp
@@ -0,0 +1,24 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class	Harl {
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+
+	public:
+		/* Constructor Method */
+		Harl( void );
+
+		/* Destructor Method */
+		~Harl( void );
+
+		/* Public Methods */
+		void	complain( std::string level );
+};
+
+#endif
