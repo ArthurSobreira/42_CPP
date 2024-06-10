a1b69bbd7d5cc2aadecf53faf4421afb1b778cd9
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 11:52:36 -0300

    Update main.cpp

--- Module01/ex01/src/main.cpp
+++ Module01/ex01/src/main.cpp
@@ -1,12 +1,43 @@
 #include "Defines.hpp"
 #include "Zombie.hpp"
 
-int	main( void ) {
-	Zombie	*horde = zombieHorde(5, "Zombie");
-
-	for (int i = 0; i < 5; i++) {
-		horde[i].announce();
+namespace Tests {
+	void	repeatChar( char c, int n, std::string color ) {
+		for (int i = 0; i < n; i++) {
+			std::cout << color << c;
+		}
+		std::cout << RESET << std::endl;
 	}
-	delete [] horde;
+
+	void	testZombieHorde( void ) {
+		Zombie	*zHorde = zombieHorde(5, "NecroKnight");
+
+		for (int i = 0; i < 5; i++) {
+			zHorde[i].announce();
+		}
+		delete [] zHorde;
+	}
+
+	void	testZombieHordeLeak( void ) {
+		Zombie	*zHorde = zombieHorde(5, "NecroNinja");
+
+		for (int i = 0; i < 5; i++) {
+			zHorde[i].announce();
+		}
+		delete [] zHorde;  // Comment this line to leak memory
+	}
+}
+
+int	main( void ) {
+	Tests::repeatChar('=', 30, WHITE);
+	std::cout << "     Testing Zombie Horde" << std::endl;
+	Tests::repeatChar('=', 30, WHITE);
+	Tests::testZombieHorde();
+
+	Tests::repeatChar('=', 33, WHITE);
+	std::cout << "     Testing Zombie Horde Leak" << std::endl;
+	Tests::repeatChar('=', 33, WHITE);
+	Tests::testZombieHordeLeak();
+
 	return (0);
 }
