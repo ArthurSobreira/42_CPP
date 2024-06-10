db8e447d8b4a0740595780e0b52ee2e1c9000bf9
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 11:52:19 -0300

    Update zombieHorde.cpp

--- Module01/ex01/src/zombieHorde.cpp
+++ Module01/ex01/src/zombieHorde.cpp
@@ -1,10 +1,10 @@
 #include "Zombie.hpp"
 
 Zombie* zombieHorde( int N, std::string name) {
-	Zombie	*horde = new Zombie[N];
+	Zombie	*zHorde = new Zombie[N];
 
 	for (int i = 0; i < N; i++) {
-		horde[i].setName(name);
+		zHorde[i].setName(name);
 	}
-	return (horde);
+	return (zHorde);
 }
