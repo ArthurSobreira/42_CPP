3d87d146252a9e442a041fd1c2399eba314a5b6d
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 15:36:26 -0300

    Update main.cpp

--- Module01/ex04/src/main.cpp
+++ Module01/ex04/src/main.cpp
@@ -5,13 +5,18 @@ int main(int argc, char **argv)
 {
 	if (argc != 4) {
 		std::cout << ERROR_INVALID_ARGUMENTS << std::endl;
-		return 1;
+		return (1);
 	}
 	std::string	filename = argv[1];
 	std::string	target = argv[2];
 	std::string	replacement = argv[3];
 
+	if (target.empty() || replacement.empty()) {
+		std::cout << ERROR_NULL_ARGUMENT << std::endl;
+		return (1);
+	}
+
 	Sed is_for_losers(target, replacement);
 	is_for_losers.replace(filename);
-	return 0;
+	return (0);
 }
