3b07744444156daaec83d2e0e81897fe56e9fd43
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 13:21:37 -0300

    Update HumanA.cpp

---
+++ Module01/ex03/src/HumanA.cpp
@@ -0,0 +1,11 @@
+#include "HumanA.hpp"
+#include "Defines.hpp"
+
+HumanA::HumanA( std::string name, Weapon& weapon ) : _name(name), _weapon(weapon) {};
+
+HumanA::~HumanA( void ) {};
+
+void	HumanA::attack( void ) {
+	std::cout << MAGENTA << this->_name << RESET << " attacks with their " << 
+	RED << this->_weapon.getType() << RESET << std::endl;
+}
