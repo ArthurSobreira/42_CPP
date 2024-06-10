5748c2dee14572fb3bad1a568b5822e2d1b843d0
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 15:11:51 -0300

    Update Sed.hpp

---
+++ Module01/ex04/includes/Sed.hpp
@@ -0,0 +1,30 @@
+#ifndef SED_HPP
+# define SED_HPP
+
+#include <iostream>
+
+class	Sed {
+	private:
+		std::string	_target;
+		std::string	_replacement;
+
+	public:
+		/* Constructor Method */
+		Sed( std::string target, std::string replacement );
+
+		/* Destructor Method */
+		~Sed( void );
+
+		/* Getter and Setter _target */
+		const	std::string	getTarget( void ) const;
+		void	setTarget( std::string target );
+
+		/* Getter and Setter _replacement */
+		const	std::string	getReplacement( void ) const;
+		void	setReplacement( std::string replacement );
+
+		/* Public Methods */
+		void	replace( std::string filename );
+};
+
+#endif
