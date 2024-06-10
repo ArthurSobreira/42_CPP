6e1c3df70248a461fe8fb29e492310bf11cb974a
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 16:53:27 -0300

    Update Sed.cpp

--- Module01/ex04/src/Sed.cpp
+++ Module01/ex04/src/Sed.cpp
@@ -1,6 +1,7 @@
 #include "Defines.hpp"
 #include "Sed.hpp"
 #include <fstream>
+#include <sstream>
 
 Sed::Sed( std::string target, std::string replacement ) : 
 	_target(target), _replacement(replacement) {};
@@ -26,11 +27,45 @@ void	Sed::setReplacement( std::string replacement ) {
 }
 
 /* Public Methods */
-void	Sed::replace( std::string filename ) {
-	std::ifstream	file(filename.c_str());
+namespace SUtils {
+	std::string getFileContent( std::ifstream &file ) {
+		std::stringstream	buffer;
+		std::string	content;
 
-	if (!file || file.fail()) {
-		std::cout << ERROR_OPEN_FILE(filename) << std::endl;
-		return ;
+		buffer << file.rdbuf();
+		content = buffer.str();
+		return (content);
 	}
+
+	void	replaceString( std::string &content, std::string target, std::string replacement ) {
+		size_t	pos = content.find(target, 0);
+		while (pos != std::string::npos) {
+			std::string before = content.substr(0, pos);
+			std::string after = content.substr(pos + target.length());
+			content = before + replacement + after;
+			pos += replacement.length();
+			pos = content.find(target, pos);
+		}
+	}
+}
+
+int	Sed::replace( std::string filename ) {
+	std::ifstream	inFile(filename.c_str());
+	std::ofstream	outFile((filename + ".replace").c_str());
+
+	if (!inFile || inFile.fail()) {
+		std::cout << ERROR_OPEN_FILE(filename) << std::endl;
+		return (FAILURE);
+	}
+	if (!outFile || outFile.fail()) {
+		std::cout << ERROR_OPEN_FILE(filename + ".replace") << std::endl;
+		return (FAILURE);
+	}
+	std::string	content = SUtils::getFileContent(inFile);
+	SUtils::replaceString(content, this->getTarget(), 
+		this->getReplacement());
+	outFile << content;
+	inFile.close();
+	outFile.close();
+	return (SUCCESS);
 }
