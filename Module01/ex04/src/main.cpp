3b24607e07df96545e00e52fcf6462f2f6aaf033
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 15:31:23 -0300

    Update main.cpp

---
+++ Module01/ex04/src/main.cpp
@@ -0,0 +1,17 @@
+#include "Defines.hpp"
+#include "../includes/Sed.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 4) {
+		std::cout << ERROR_INVALID_ARGUMENTS << std::endl;
+		return 1;
+	}
+	std::string	filename = argv[1];
+	std::string	target = argv[2];
+	std::string	replacement = argv[3];
+
+	Sed is_for_losers(target, replacement);
+	is_for_losers.replace(filename);
+	return 0;
+}
