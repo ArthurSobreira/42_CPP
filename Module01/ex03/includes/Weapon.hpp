82599e73344834147ee47bbefb23b75148730c29
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 12:48:14 -0300

    Update Weapon.hpp

---
+++ Module01/ex03/includes/Weapon.hpp
@@ -0,0 +1,22 @@
+#ifndef WEAPON_CPP
+# define WEAPON_CPP
+
+#include <iostream>
+
+class	Weapon {
+	private:
+		std::string _type;
+
+	public:
+		/* Constructor Method */
+		Weapon( std::string type );
+
+		/* Destructor Method */
+		~Weapon( void );
+
+		/* Getter and Setter _type */
+		const	std::string	getType( void ) const;
+		void	setType( std::string type );
+}
+
+#endif
