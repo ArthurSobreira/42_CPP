807a7db184f073700c81648c867e87a3998d7254
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-10 11:04:45 -0300

    Update Zombie.cpp

---
+++ Module01/ex01/src/Zombie.cpp
@@ -0,0 +1,26 @@
+#include "Defines.hpp"
+#include "Zombie.hpp"
+
+Zombie::Zombie( void ) {
+	std::cout << CYAN << "Zombie constructor called" \
+		<< RESET << std::endl;
+}
+
+Zombie::~Zombie( void ) {
+	std::cout << RED << "Zombie " << this->getName() << " destroyed" \
+		<< RESET << std::endl << std::endl;
+}
+
+/* Getter and Setter _name */
+std::string	Zombie::getName( void ) const {
+	return (this->_name);
+}
+
+void	Zombie::setName( std::string name ) {
+	this->_name = name;
+}
+
+void	Zombie::announce( void ) {
+	std::cout << GREEN << this->getName() << ": BraiiiiiiinnnzzzZ..." \
+		<< RESET << std::endl;
+}
