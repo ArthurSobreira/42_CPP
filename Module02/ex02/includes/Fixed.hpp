caa0eb5c6488b6c118798ae4af48e49d4846d05c
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-11 15:07:41 -0300

    Update Fixed.hpp

--- Module02/ex02/includes/Fixed.hpp
+++ Module02/ex02/includes/Fixed.hpp
@@ -18,10 +18,10 @@ class Fixed {
 		Fixed( const Fixed &other );
 
 		/* Copy Assignment Operator Overload */
-		Fixed &operator=(const Fixed &other);
+		Fixed &operator=( const Fixed &other );
 
 		/* Destructor Method */
-		~Fixed();
+		~Fixed( void );
 
 		/* Public Methods */
 		int		getRawBits( void ) const;
@@ -30,32 +30,32 @@ class Fixed {
 		int		toInt( void ) const;
 
 		/* Comparison Operators */
-		bool	operator>(Fixed const &other) const;
-		bool	operator<(Fixed const &other) const;
-		bool	operator>=(Fixed const &other) const;
-		bool	operator<=(Fixed const &other) const;
-		bool	operator==(Fixed const &other) const;
-		bool	operator!=(Fixed const &other) const;
+		bool	operator>( Fixed const &other ) const;
+		bool	operator<( Fixed const &other ) const;
+		bool	operator>=( Fixed const &other ) const;
+		bool	operator<=( Fixed const &other ) const;
+		bool	operator==( Fixed const &other ) const;
+		bool	operator!=( Fixed const &other ) const;
 
 		/* Arithmetic Operators */
-		Fixed	operator+(Fixed const &other) const;
-		Fixed	operator-(Fixed const &other) const;
-		Fixed	operator*(Fixed const &other) const;
-		Fixed	operator/(Fixed const &other) const;
+		Fixed	operator+( Fixed const &other ) const;
+		Fixed	operator-( Fixed const &other ) const;
+		Fixed	operator*( Fixed const &other ) const;
+		Fixed	operator/( Fixed const &other ) const;
 
 		/* Increment and Decrement Operators */
-		Fixed	&operator++(void);
-		Fixed	operator++(int);
-		Fixed	&operator--(void);
-		Fixed	operator--(int);
+		Fixed	&operator++( void );
+		Fixed	operator++( int );
+		Fixed	&operator--( void );
+		Fixed	operator--( int );
 
 		/* Min and Max */
-		static Fixed	&min(Fixed &a, Fixed &b);
-		static Fixed	&max(Fixed &a, Fixed &b);
-		static Fixed const	&min(Fixed const &a, Fixed const &b);
-		static Fixed const	&max(Fixed const &a, Fixed const &b);
+		static Fixed	&min( Fixed &a, Fixed &b );
+		static Fixed	&max( Fixed &a, Fixed &b );
+		static Fixed const	&min( Fixed const &a, Fixed const &b );
+		static Fixed const	&max( Fixed const &a, Fixed const &b );
 };
 
-std::ostream &operator<<(std::ostream &out, Fixed const &value);
+std::ostream &operator<<( std::ostream &out, Fixed const &value );
 
 #endif
