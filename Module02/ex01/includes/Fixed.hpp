9ea54562b76145f8fca102879ec613b3ff96062c
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-11 07:04:00 -0300

    Update Fixed.hpp

---
+++ Module02/ex01/includes/Fixed.hpp
@@ -0,0 +1,35 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int	_fixedPointValue;
+		static const int _fractionalBits = 8;
+
+	public:
+		/* Constructor Method */
+		Fixed( void );
+		Fixed( int const value );
+		Fixed( float const value );
+
+		/* Copy Constructor Method */
+		Fixed( const Fixed &other );
+
+		/* Copy Assignment Operator Overload */
+		Fixed &operator=(const Fixed &other);
+
+		/* Destructor Method */
+		~Fixed();
+
+		/* Public Methods */
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+};
+
+std::ostream &operator<<(std::ostream &out, Fixed const &value);
+
+#endif
