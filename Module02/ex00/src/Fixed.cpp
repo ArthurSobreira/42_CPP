cf4628c492a64ca5681b5e73d1037f9825fe2ed1
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-11 15:04:19 -0300

    Update Fixed.cpp

--- Module02/ex00/src/Fixed.cpp
+++ Module02/ex00/src/Fixed.cpp
@@ -12,7 +12,7 @@ Fixed::Fixed( const Fixed &other ) {
 }
 
 /* Copy Assignment Operator Overload */
-Fixed &Fixed::operator=(const Fixed &other) {
+Fixed &Fixed::operator=( const Fixed &other ) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 		this->_fixedPointValue = other.getRawBits();
