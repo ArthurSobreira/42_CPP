de2f0720b136c362a452d2e5dce9a43e80d4c16b
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-11 16:30:36 -0300

    Update Point.hpp

---
+++ Module02/ex03/includes/Point.hpp
@@ -0,0 +1,30 @@
+#ifndef POINT_HPP
+# define POINT_HPP
+
+#include "Fixed.hpp"
+
+class Point {
+	private:
+		Fixed const	_x;
+		Fixed const	_y;
+
+	public:
+		/* Constructor Methods */
+		Point( void );
+		Point( const float x, const float y );
+
+		/* Copy Constructor Method */
+		Point( const Point &other );
+
+		/* Copy Assignment Operator Overload */
+		Point &operator=(const Point &other);
+
+		/* Destructor Method */
+		~Point();
+
+		/* Getter Methods */
+		Fixed const	getX( void ) const;
+		Fixed const	getY( void ) const;
+};
+
+#endif
