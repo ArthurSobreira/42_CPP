c726c5519bc41a0d4adc5e8d9986ad6f87fc4b52
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-11 19:01:21 -0300

    Update main.cpp

--- Module02/ex03/src/main.cpp
+++ Module02/ex03/src/main.cpp
@@ -18,18 +18,22 @@ namespace Tests {
 		return string.str();
 	}
 
+	void	validate( Point const a, Point const b, Point const c, Point const point ) {
+		std::cout << "a is " << printCoordinates(a) << std::endl;
+		std::cout << "b is " << printCoordinates(b) << std::endl;
+		std::cout << "c is " << printCoordinates(c) << std::endl;
+		std::cout << "point is " << printCoordinates(point) << std::endl;
+		std::cout << "Is point inside triangle? " << \
+		(bsp(a, b, c, point) ? YES : NO) << std::endl;
+	}
+
 	void	testPointInsideTriangle( void ) {
 		Point const a( 0, 0 );
 		Point const b( 4, 0 );
 		Point const c( 2, 4 );
 		Point const point( 2, 2 );
 
-		std::cout << "a is " << printCoordinates(a) << std::endl;
-		std::cout << "b is " << printCoordinates(b) << std::endl;
-		std::cout << "c is " << printCoordinates(c) << std::endl;
-		std::cout << "point is " << printCoordinates(point) << std::endl;
-		std::cout << "Is point inside triangle? " << \
-		(bsp(a, b, c, point) ? "Yes" : "No") << std::endl;
+		validate(a, b, c, point);
 	}
 
 	void	testPointOutsideTriangle( void ) {
@@ -38,42 +42,47 @@ namespace Tests {
 		Point const c( 2, 4 );
 		Point const point( 2, 5 );
 
-		std::cout << "a is " << printCoordinates(a) << std::endl;
-		std::cout << "b is " << printCoordinates(b) << std::endl;
-		std::cout << "c is " << printCoordinates(c) << std::endl;
-		std::cout << "point is " << printCoordinates(point) << std::endl;
-		std::cout << "Is point inside triangle? " << \
-		(bsp(a, b, c, point) ? "Yes" : "No") << std::endl;
+		validate(a, b, c, point);
 	}
 
-	void	testPointOnTriangle( void ) {
-		Point const a( 0, 0 );
-		Point const b( 4, 0 );
-		Point const c( 2, 4 );
-		Point const point( 2, 0 );
+	void	testPointOnLine( void ) {
+		Point const a( -2, -2 );
+		Point const b( 0, 3 );
+		Point const c( 2, -2 );
+		Point const point( 1, -2 );
 
-		std::cout << "a is " << printCoordinates(a) << std::endl;
-		std::cout << "b is " << printCoordinates(b) << std::endl;
-		std::cout << "c is " << printCoordinates(c) << std::endl;
-		std::cout << "point is " << printCoordinates(point) << std::endl;
-		std::cout << "Is point inside triangle? " << \
-		(bsp(a, b, c, point) ? "Yes" : "No") << std::endl;
+		validate(a, b, c, point);
+	}
+
+	void testPointOnEdge( void ) {
+		Point const a( -2, -2 );
+		Point const b( 0, 3 );
+		Point const c( 2, -2 );
+		Point const point( 0, 3 );
+
+		validate(a, b, c, point);
 	}
 }
 
 int main( void ) {
-	Tests::repeatChar('=', 40, YELLOW);
+	Tests::repeatChar('=', 36, YELLOW);
 	std::cout << "     Test Point Inside Triangle" << std::endl;
-	Tests::repeatChar('=', 40, YELLOW);
+	Tests::repeatChar('=', 36, YELLOW);
 	Tests::testPointInsideTriangle();
 
-	Tests::repeatChar('=', 40, YELLOW);
+	Tests::repeatChar('=', 37, YELLOW);
 	std::cout << "     Test Point Outside Triangle" << std::endl;
-	Tests::repeatChar('=', 40, YELLOW);
+	Tests::repeatChar('=', 37, YELLOW);
 	Tests::testPointOutsideTriangle();
 
-	Tests::repeatChar('=', 40, YELLOW);
-	std::cout << "     Test Point On Triangle" << std::endl;
-	Tests::repeatChar('=', 40, YELLOW);
+	Tests::repeatChar('=', 28, YELLOW);
+	std::cout << "     Test Point On Line" << std::endl;
+	Tests::repeatChar('=', 28, YELLOW);
+	Tests::testPointOnLine();
+
+	Tests::repeatChar('=', 28, YELLOW);
+	std::cout << "     Test Point On Edge" << std::endl;
+	Tests::repeatChar('=', 28, YELLOW);
+	Tests::testPointOnEdge();
 	return 0;
 }
