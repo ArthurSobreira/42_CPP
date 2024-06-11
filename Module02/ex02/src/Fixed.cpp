56bec0273408a697a43b9926bb94a35f583166dc
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-11 15:32:06 -0300

    Update Fixed.cpp

--- Module02/ex02/src/Fixed.cpp
+++ Module02/ex02/src/Fixed.cpp
@@ -2,38 +2,30 @@
 #include <cmath>
 
 /* Constructor Methods */
-Fixed::Fixed( void ) : _fixedPointValue(0) {
-	std::cout << "Default constructor called" << std::endl;
-}
+Fixed::Fixed( void ) : _fixedPointValue(0) {};
 
 Fixed::Fixed( int const value ) {
-	std::cout << "Int constructor called" << std::endl;
 	_fixedPointValue = value << _fractionalBits;
 }
 
 Fixed::Fixed( float const value ) {
-	std::cout << "Float constructor called" << std::endl;
 	_fixedPointValue = static_cast<int>(roundf(value * (1 << _fractionalBits)));
 }
 
 /* Copy Constructor Method */
 Fixed::Fixed( const Fixed &other ) {
-	std::cout << "Copy constructor called" << std::endl;
 	*this = other;
 }
 
 /* Copy Assignment Operator Overload */
 Fixed &Fixed::operator=( const Fixed &other ) {
-	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 		this->_fixedPointValue = other.getRawBits();
 	return (*this);
 }
 
 /* Destructor Method */
-Fixed::~Fixed( void ) {
-	std::cout << "Destructor called" << std::endl;
-}
+Fixed::~Fixed( void ) {};
 
 /* Getter and Setter RawBits */
 int	Fixed::getRawBits( void ) const {
