766e7e448803ed59151cd0f2dacc11d3410185fb
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-11 16:37:09 -0300

    Update Point.cpp

---
+++ Module02/ex03/src/Point.cpp
@@ -0,0 +1,32 @@
+#include "Point.hpp"
+
+/* Constructor Methods */
+Point::Point( void ) : _x(0), _y(0) {};
+
+Point::Point( const float x, const float y ) : _x(x), _y(y) {};
+
+/* Copy Constructor Method */
+Point::Point( const Point &other ) {
+	*this = other;
+}
+
+/* Copy Assignment Operator Overload */
+Point &Point::operator=( const Point &other ) {
+	if (this != &other) {
+		this->_x = other.getX();
+		this->_y = other.getY();
+	}
+	return (*this);
+}
+
+/* Destructor Method */
+Point::~Point() {};
+
+/* Getter Methods */
+Fixed const	Point::getX( void ) const {
+	return (this->_x);
+}
+
+Fixed const Point::getY( void ) const {
+	return (this->_y);
+}
