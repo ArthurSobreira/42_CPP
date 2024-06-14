107804f3fa8b73ae7428ecba68ad270e92dc6234
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 10:54:05 -0300

    Update Dog.cpp

--- Module04/ex01/src/Dog.cpp
+++ Module04/ex01/src/Dog.cpp
@@ -5,11 +5,13 @@
 Dog::Dog( void ) : Animal() {
 	std::cout << "Dog Default Constructor called" << std::endl;
 	this->setType("Dog");
+	this->_brain = new Brain();
 }
 
 /* Copy Constructor Method */
 Dog::Dog( const Dog &other ) : Animal(other) {
 	std::cout << "Dog Copy Constructor called" << std::endl;
+	this->_brain = new Brain();
 	*this = other;
 }
 
@@ -18,6 +20,7 @@ Dog &Dog::operator=( const Dog &other ) {
 	std::cout << "Dog Copy Assignment Operator called" << std::endl;
 	if (this != &other) {
 		this->_type = other.getType();
+		*this->_brain = *other._brain;
 	}
 	return (*this);
 }
@@ -25,9 +28,14 @@ Dog &Dog::operator=( const Dog &other ) {
 /* Destructor Method */
 Dog::~Dog( void ) {
 	std::cout << "Dog Destructor called" << std::endl;
+	delete this->_brain;
 }
 
 /* Public Method */
 void	Dog::makeSound( void ) const {
 	std::cout << DOG_SOUND_MESSAGE(this->getType()) << std::endl;
 }
+
+Brain *Dog::getBrain( void ) const {
+	return (this->_brain);
+}	
