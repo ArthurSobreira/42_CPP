3e6e83c0c27e0de04c209f14f62fe430f0698a7c
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 15:00:45 -0300

    Update Cat.hpp

--- Module04/ex02/includes/Cat.hpp
+++ Module04/ex02/includes/Cat.hpp
@@ -1,11 +1,11 @@
 #ifndef CAT_HPP
 #define CAT_HPP
 
-#include "Animal.hpp"
+#include "AAnimal.hpp"
 #include "Brain.hpp"
 #include <iostream>
 
-class Cat : public Animal {
+class Cat : public AAnimal {
 	private:
 		Brain *_brain;
 
