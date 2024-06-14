f0e869d7e90bfa33510f9bf841105f5b3e0cf66e
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 16:46:19 -0300

    Update MateriaSource.hpp

---
+++ Module04/ex03/includes/MateriaSource.hpp
@@ -0,0 +1,30 @@
+
+#ifndef MATERIASOURCE_HPP
+#define MATERIASOURCE_HPP
+
+#include "IMateriaSource.hpp"
+#include "AMateria.hpp"
+
+class MateriaSource {
+	private:
+		AMateria	*_materia[4];
+
+	public:
+		/* Constructor Method */
+		MateriaSource( void );
+
+		/* Copy Constructor Method */
+		MateriaSource( const MateriaSource &other );
+
+		/* Copy Assignment Operator Overload */
+		MateriaSource &operator=( const MateriaSource &other );
+
+		/* Destructor Method */
+		~MateriaSource( void );
+
+		/* Public Method */
+		void learnMateria( AMateria *m );
+		AMateria *createMateria( std::string const &type );
+};
+
+#endif // MATERIASOURCE_HPP_
