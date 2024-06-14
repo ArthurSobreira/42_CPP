a89318fbd3041b4aa5330b7077759aa0241d3bd9
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 16:24:13 -0300

    Update Character.hpp

---
+++ Module04/ex03/includes/Character.hpp
@@ -0,0 +1,36 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include "ICharacter.hpp"
+#include "AMateria.hpp
+#include <iostream>
+
+class Character {
+	private:
+		std::string _name;
+		AMateria *_inventory[4];
+		
+	public:
+		/* Constructor Method */
+		Character( void );
+		Character( std::string const &name );
+
+		/* Copy Constructor Method */
+		Character( const Character &other );
+
+		/* Copy Assignment Operator Overload */
+		Character &operator=( const Character &other );
+
+		/* Destructor Method */
+		~Character( void );
+
+		/* Getter _name Method */
+		std::string const &getName( void ) const;
+
+		/* Public Method */
+		void equip( AMateria *m );
+		void unequip( int idx );
+		void use( int idx, ICharacter &target );
+};
+
+#endif // CHARACTER_HPP_
