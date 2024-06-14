2ad661eed44874f8900ec1ac69dfef1bf759b6fa
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 02:25:12 -0300

    Update includes folder

---
+++ Module03/ex03/includes/ScavTrap.hpp
@@ -0,0 +1,27 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+class ScavTrap : public ClapTrap {
+	public:
+		/* Constructor Methods */
+		ScavTrap( void );
+		ScavTrap( const std::string &name );
+
+		/* Copy Constructor Method */
+		ScavTrap( const ScavTrap &other );
+
+		/* Copy Assignment Operator Overload */
+		ScavTrap &operator=( const ScavTrap &other );
+
+		/* Destructor Method */
+		~ScavTrap( void );
+
+		/* Public Methods */
+		void	attack( const std::string &target );
+		void	guardGate( void );
+};
+
+#endif
