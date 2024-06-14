125a8875e05fcdcc7c738e2b4bc2c58128e5ed73
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 15:58:00 -0300

    Update Cure.hpp

---
+++ Module04/ex03/includes/Cure.hpp
@@ -0,0 +1,26 @@
+#ifndef CURE_HPP
+#define CURE_HPP
+
+#include "AMateria.hpp"
+#include "ICharacter.hpp"
+
+class Cure : public AMateria {
+	public:
+		/* Constructor Method */
+		Cure( void );
+
+		/* Copy Constructor Method */
+		Cure( const Cure &other );
+
+		/* Copy Assignment Operator Overload */
+		Cure &operator=( const Cure &other );
+
+		/* Destructor Method */
+		~Cure( void );
+
+		/* Public Method */
+		AMateria *clone( void ) const;
+		void use( ICharacter &target );
+};
+
+#endif
