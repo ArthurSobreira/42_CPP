c9e67d44404c8615373a3e36314c767ede4c41f7
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 09:02:48 -0300

    Update WrongCat.hpp

---
+++ Module04/ex00/includes/WrongCat.hpp
@@ -0,0 +1,25 @@
+#ifndef WRONGCAT_HPP
+#define WRONGCAT_HPP
+
+#include "WrongAnimal.hpp"
+#include <iostream>
+
+class WrongCat : public WrongAnimal {
+	public:
+		/* Constructor Method */
+		WrongCat( void );
+
+		/* Copy Constructor Method */
+		WrongCat( const Cat &other );
+
+		/* Copy Assignment Operator Overload */
+		WrongCat &operator=( const WrongCat &other );
+
+		/* Destructor Method */
+		~WrongCat( void );
+
+		/* Public Method */
+		void makeSound( void ) const;
+};
+
+#endif
