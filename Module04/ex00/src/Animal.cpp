551117b5c8f938e5676fbe91120f5b05ca585c45
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 07:40:22 -0300

    Update Animal.cpp

---
+++ Module04/ex00/src/Animal.cpp
@@ -0,0 +1,37 @@
+#include "Defines.hpp"
+#include "Animal.hpp"
+
+/* Constructor Method */
+Animal::Animal( void ) : _type("Animal") {
+	std::cout << "Animal Default Constructor called" << std::endl;
+}
+
+/* Copy Constructor Method */
+Animal::Animal( const Animal &other ) {
+	std::cout << "Animal Copy Constructor called" << std::endl;
+	*this = other;
+}
+
+/* Copy Assignment Operator Overload */
+Animal &Animal::operator=( const Animal &other ) {
+	std::cout << "Animal Copy Assignment Operator called" << std::endl;
+	if (this != &other) {
+		this->_type = other.getType();
+	}
+	return (*this);
+}
+
+/* Destructor Method */
+Animal::~Animal( void ) {
+	std::cout << "Animal Destructor called" << std::endl;
+}
+
+/* Getter _type Method */
+std::string Animal::getType( void ) const {
+	return (this->_type);
+}
+
+/* Public Method */
+void	Animal::makeSound( void ) const {
+	std::cout << ANIMAL_SOUND_MESSAGE(this->getType()) << std::endl;
+}
