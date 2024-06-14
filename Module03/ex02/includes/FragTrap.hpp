7e35c8f6781566ad98da17134f69df08eeafe732
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 01:35:10 -0300

    Update FragTrap.hpp

---
+++ Module03/ex02/includes/FragTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+class FragTrap : public ClapTrap {
+	public:
+		/* Constructor Methods */
+		FragTrap( void );
+		FragTrap( const std::string &name );
+
+		/* Copy Constructor Method */
+		FragTrap(const FragTrap &other);
+
+		/* Copy Assignment Operator Overload */
+		FragTrap &operator=(const FragTrap &rhs);
+
+		/* Destructor Method */
+		~FragTrap( void );
+
+		/* Public Methods */
+		void	highFivesGuys( void );
+};
+
+#endif
