00aee01f0c6199e3caec0a2fe1742ab98b4347a7
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-14 17:36:34 -0300

    Update ICharacter.hpp

--- Module04/ex03/includes/ICharacter.hpp
+++ Module04/ex03/includes/ICharacter.hpp
@@ -7,11 +7,8 @@ class AMateria;
 
 class ICharacter {
 	public:
-		/* Destructor Method */
-		virtual ~ICharacter( void ) {}
-
-		/* Public Method */
-		virtual std::string const &getName( void ) const = 0;
+		virtual ~ICharacter() {}
+		virtual std::string const & getName() const = 0;
 		virtual void equip( AMateria *m ) = 0;
 		virtual void unequip( int idx ) = 0;
 		virtual void use( int idx, ICharacter &target ) = 0;
