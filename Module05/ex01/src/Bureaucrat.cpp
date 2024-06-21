2ddd3f9a14f2b954d5865427d87e4445809f4d70
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-21 16:12:53 -0300

    Update src folder

---
+++ Module05/ex01/src/Bureaucrat.cpp
@@ -0,0 +1,73 @@
+#include "Defines.hpp"
+#include "Bureaucrat.hpp"
+
+/* Constructor Methods */
+Bureaucrat::Bureaucrat( void ) : _name("Default"), _grade(10) {}
+
+Bureaucrat::Bureaucrat( const std::string name, int grade ) : _name(name) {
+	if (grade < 1) {
+		throw GradeTooHighException();
+	} else if (grade > 150) {
+		throw GradeTooLowException();
+	} else {
+		this->_grade = grade;
+	}
+}
+
+/* Copy Constructor Method */
+Bureaucrat::Bureaucrat( const Bureaucrat &other ) {
+	*this = other;
+}
+
+/* Copy Assignment Operator Overload */
+Bureaucrat &Bureaucrat::operator=( const Bureaucrat &other ) {
+	if (this != &other) {
+		const_cast<std::string &>(this->_name) = other.getName();
+		this->_grade = other.getGrade();
+	}
+	return (*this);
+}
+
+/* Destructor Method */
+Bureaucrat::~Bureaucrat( void ) {};
+
+/* Public Methods */
+std::string	Bureaucrat::getName( void ) const {
+	return this->_name;
+}
+
+int	Bureaucrat::getGrade( void ) const {
+	return this->_grade;
+}
+
+void	Bureaucrat::incrementGrade( void ) {
+	if (this->getGrade() - 1 < 1) {
+		throw GradeTooHighException();
+	} else {
+		this->_grade--;
+	}
+}
+
+void	Bureaucrat::decrementGrade( void ) {
+	if (this->getGrade() + 1 > 150) {
+		throw GradeTooLowException();
+	} else {
+		this->_grade++;
+	}
+}
+
+/* Exception Classes */
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+	return ("Grade is too high! Only 1-150 allowed.");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+	return ("Grade is too low! Only 1-150 allowed.");
+}
+
+/* Output Operator Overload */
+std::ostream &operator<<( std::ostream &out, Bureaucrat const &value ) {
+	out << COLORIZE(CYAN, value.getName()) << ", bureaucrat grade " << \
+	COLORIZE(YELLOW, value.getGrade());
+	return (out);
+}
---
+++ Module05/ex01/src/main.cpp
@@ -0,0 +1,71 @@
+#include "Defines.hpp"
+#include "Bureaucrat.hpp"
+
+namespace Tests {
+	void	repeatChar( char c, int n, std::string color ) {
+		for (int i = 0; i < n; i++) {
+			std::cout << color << c;
+		}
+		std::cout << RESET << std::endl;
+	}
+
+	void	testValidBureaucrat( void ) {
+		try {
+			Bureaucrat b1;
+			std::cout << b1 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+		try {
+			Bureaucrat b2("Bureaucrat2", 1);
+			b2.decrementGrade();
+			std::cout << b2 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+		try {
+			Bureaucrat b3("Bureaucrat3", 150);
+			b3.incrementGrade();
+			std::cout << b3 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+	}
+
+	void	testInvalidBureaucrat( void ) {
+		try {
+			Bureaucrat b1("Bureaucrat1", -10);
+			std::cout << b1 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+		try {
+			Bureaucrat b2("Bureaucrat2", 1);
+			b2.incrementGrade();
+			std::cout << b2 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+		try {
+			Bureaucrat b3("Bureaucrat3", 150);
+			b3.decrementGrade();
+			std::cout << b3 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+	}
+}
+
+int main( void ) {
+	Tests::repeatChar('=', 42, CYAN);
+	std::cout << "          Valid Bureaucrat Tests " << std::endl;
+	Tests::repeatChar('=', 42, CYAN);
+	Tests::testValidBureaucrat();
+
+	Tests::repeatChar('=', 42, CYAN);
+	std::cout << "         Invalid Bureaucrat Tests " << std::endl;
+	Tests::repeatChar('=', 42, CYAN);
+	Tests::testInvalidBureaucrat();
+
+	return (0);
+}
