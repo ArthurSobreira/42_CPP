f916c6813d8dc555854b8cdf3d8d05e42512ea3b
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-28 14:30:33 -0300

    Update main.cpp

--- Module05/ex01/src/main.cpp
+++ Module05/ex01/src/main.cpp
@@ -1,5 +1,6 @@
 #include "Defines.hpp"
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 namespace Tests {
 	void	repeatChar( char c, int n, std::string color ) {
@@ -9,47 +10,76 @@ namespace Tests {
 		std::cout << RESET << std::endl;
 	}
 
-	void	testValidBureaucrat( void ) {
+	void	testValidForm( void ) {
 		try {
-			Bureaucrat b1;
-			std::cout << b1 << std::endl;
+			Form f1;
+			std::cout << f1 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
 		try {
-			Bureaucrat b2("Bureaucrat2", 1);
-			b2.decrementGrade();
-			std::cout << b2 << std::endl;
+			Form f2("Form1", 1, 1);
+			std::cout << f2 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
 		try {
-			Bureaucrat b3("Bureaucrat3", 150);
-			b3.incrementGrade();
-			std::cout << b3 << std::endl;
+			Form f3("Form2", 150, 150);
+			std::cout << f3 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
 	}
 
-	void	testInvalidBureaucrat( void ) {
+	void	testInvalidForm( void ) {
 		try {
-			Bureaucrat b1("Bureaucrat1", -10);
-			std::cout << b1 << std::endl;
+			Form f1("Form1", 0, 1);
+			std::cout << f1 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
 		try {
-			Bureaucrat b2("Bureaucrat2", 1);
-			b2.incrementGrade();
-			std::cout << b2 << std::endl;
+			Form f2("Form2", 151, 152);
+			std::cout << f2 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+	}
+
+	void	testValidSignForm( void ) {
+		try {
+			Bureaucrat b1("Bureaucrat1", 5);
+			Form f1("Form1", 5, 10);
+			b1.signForm(f1);
+			std::cout << f1 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
 		try {
-			Bureaucrat b3("Bureaucrat3", 150);
-			b3.decrementGrade();
-			std::cout << b3 << std::endl;
+			Bureaucrat b2("Bureaucrat2", 120);
+			Form f2("Form2", 150, 150);
+			b2.signForm(f2);
+			std::cout << f2 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+	}
+
+	void	testInvalidSignForm( void ) {
+		try {
+			Bureaucrat b1("Bureaucrat1", 10);
+			Form f1("Form1", 10, 10);
+			b1.decrementGrade();
+			b1.signForm(f1);
+			std::cout << f1 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+		try {
+			Bureaucrat b2("Bureaucrat2", 120);
+			Form f2("Form2", 100, 100);
+			b2.signForm(f2);
+			std::cout << f2 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
@@ -57,15 +87,28 @@ namespace Tests {
 }
 
 int main( void ) {
-	Tests::repeatChar('=', 42, CYAN);
-	std::cout << "          Valid Bureaucrat Tests " << std::endl;
-	Tests::repeatChar('=', 42, CYAN);
-	Tests::testValidBureaucrat();
+	Tests::repeatChar('=', 44, CYAN);
+	std::cout << "              Valid Form Tests " << std::endl;
+	Tests::repeatChar('=', 44, CYAN);
+	Tests::testValidForm();
 
-	Tests::repeatChar('=', 42, CYAN);
-	std::cout << "         Invalid Bureaucrat Tests " << std::endl;
-	Tests::repeatChar('=', 42, CYAN);
-	Tests::testInvalidBureaucrat();
+	std::cout << std::endl;
+	Tests::repeatChar('=', 44, CYAN);
+	std::cout << "             Invalid Form Tests " << std::endl;
+	Tests::repeatChar('=', 44, CYAN);
+	Tests::testInvalidForm();
+
+	std::cout << std::endl;
+	Tests::repeatChar('=', 44, CYAN);
+	std::cout << "            Valid Sign Form Tests " << std::endl;
+	Tests::repeatChar('=', 44, CYAN);
+	Tests::testValidSignForm();
+
+	std::cout << std::endl;
+	Tests::repeatChar('=', 44, CYAN);
+	std::cout << "           Invalid Sign Form Tests " << std::endl;
+	Tests::repeatChar('=', 44, CYAN);
+	Tests::testInvalidSignForm();
 
 	return (0);
 }
