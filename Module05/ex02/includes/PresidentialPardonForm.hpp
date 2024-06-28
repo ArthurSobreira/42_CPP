e622d75ec784caa5ab92e459d1f5495edd98a2f9
Arthur Sobreira <tutusobreira@gmail.com>
2024-06-28 19:31:00 -0300

    Update PresidentialPardonForm.hpp

---
+++ Module05/ex02/includes/PresidentialPardonForm.hpp
@@ -0,0 +1,28 @@
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm {
+	private:
+		std::string const _target;
+		
+	public:
+		/* Constructor Method */
+		PresidentialPardonForm( void );
+		
+		/* Copy Constructor Method */
+		PresidentialPardonForm( const PresidentialPardonForm &other );
+		
+		/* Copy Assignment Operator Overload */
+		PresidentialPardonForm &operator=( const PresidentialPardonForm &other );
+		
+		/* Destructor Method */
+		~PresidentialPardonForm( void );
+		
+		/* Public Methods */
+		std::string getTarget( void ) const;
+		void        execute( Bureaucrat const &executor ) const;
+};
+
+#endif
