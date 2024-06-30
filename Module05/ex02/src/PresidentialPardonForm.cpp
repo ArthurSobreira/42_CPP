eb2ab8867605b222912d889cff4e74dd5f81725f
Arthur <tutusobreira@gmail.com>
2024-06-30 17:57:45 -0300

    Update PresidentialPardonForm.cpp

--- Module05/ex02/src/PresidentialPardonForm.cpp
+++ Module05/ex02/src/PresidentialPardonForm.cpp
@@ -1,3 +1,4 @@
+#include "Defines.hpp"
 #include "PresidentialPardonForm.hpp"
 
 /* Constructor Method */
@@ -8,14 +9,16 @@ PresidentialPardonForm::PresidentialPardonForm( std::string const target ) : \
 	AForm("PresidentialPardonForm", 25, 5), _target(target) {}
 
 /* Copy Constructor Method */
-PresidentialPardonForm::PresidentialPardonForm( const PresidentialPardonForm &other ) {
+PresidentialPardonForm::PresidentialPardonForm( const PresidentialPardonForm &other ) : \
+	AForm("PresidentialPardonForm", 25, 5), _target(other.getTarget()) {
 	*this = other;
 }
 
 /* Copy Assignment Operator Overload */
 PresidentialPardonForm &PresidentialPardonForm::operator=( const PresidentialPardonForm &other ) {
 	if ( this != &other ) {
-		this->attribute = other.getAttribute();
+		AForm::operator=(other);
+		const_cast<std::string &>(this->_target) = other.getTarget();
 	}
 	return (*this);
 }
@@ -24,10 +27,16 @@ PresidentialPardonForm &PresidentialPardonForm::operator=( const PresidentialPar
 PresidentialPardonForm::~PresidentialPardonForm( void ) {}
 
  /* Public Methods */
-int PresidentialPardonForm::getAttribute( void ) const {
-	return (this->attribute);
+std::string PresidentialPardonForm::getTarget( void ) const {
+	return (this->_target);
 }
 
-void PresidentialPardonForm::setAttribute( int value ) {
-	this->attribute = value;
+void PresidentialPardonForm::execute( Bureaucrat const &executor ) const {
+	if (!this->getSigned()) {
+		throw AForm::FormNotSignedException();
+	} else if (executor.getGrade() > this->getGradeToExecute()) {
+		throw AForm::GradeTooLowException();
+	}
+	std::cout << COLORIZE(CYAN, this->getTarget()) << \
+	" has been pardoned by Zaphod Beeblebrox." << std::endl;
 }
