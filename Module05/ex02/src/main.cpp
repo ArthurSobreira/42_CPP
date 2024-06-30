1f121c1df3affa68621dda7b7100433eafef5fdc
Arthur <tutusobreira@gmail.com>
2024-06-30 18:04:32 -0300

    Update main.cpp

--- Module05/ex02/src/main.cpp
+++ Module05/ex02/src/main.cpp
@@ -1,6 +1,7 @@
 #include "Defines.hpp"
 #include "Bureaucrat.hpp"
-#include "Form.hpp"
+#include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 namespace Tests {
 	void	repeatChar( char c, int n, std::string color ) {
@@ -10,76 +11,56 @@ namespace Tests {
 		std::cout << RESET << std::endl;
 	}
 
-	void	testValidForm( void ) {
+	void	testValidPresidentialPardonForm( void ) {
 		try {
-			Form f1;
-			std::cout << f1 << std::endl;
+			PresidentialPardonForm ppf1;
+			std::cout << ppf1 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
 		try {
-			Form f2("Form1", 1, 1);
-			std::cout << f2 << std::endl;
+			PresidentialPardonForm ppf2("Target1");
+			std::cout << ppf2 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
 		try {
-			Form f3("Form2", 150, 150);
-			std::cout << f3 << std::endl;
-		} catch (const std::exception &e) {
-			std::cout << COLORIZE(RED, e.what()) << std::endl;
-		}
-	}
-
-	void	testInvalidForm( void ) {
-		try {
-			Form f1("Form1", 0, 1);
-			std::cout << f1 << std::endl;
-		} catch (const std::exception &e) {
-			std::cout << COLORIZE(RED, e.what()) << std::endl;
-		}
-		try {
-			Form f2("Form2", 151, 152);
-			std::cout << f2 << std::endl;
-		} catch (const std::exception &e) {
-			std::cout << COLORIZE(RED, e.what()) << std::endl;
-		}
-	}
-
-	void	testValidSignForm( void ) {
-		try {
+			PresidentialPardonForm ppf3("Target2");
 			Bureaucrat b1("Bureaucrat1", 5);
-			Form f1("Form1", 5, 10);
-			b1.signForm(f1);
-			std::cout << f1 << std::endl;
-		} catch (const std::exception &e) {
-			std::cout << COLORIZE(RED, e.what()) << std::endl;
-		}
-		try {
-			Bureaucrat b2("Bureaucrat2", 120);
-			Form f2("Form2", 150, 150);
-			b2.signForm(f2);
-			std::cout << f2 << std::endl;
+			ppf3.beSigned(b1);
+			b1.executeForm(ppf3);
+			std::cout << ppf3 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
 	}
 
-	void	testInvalidSignForm( void ) {
+	void	testInvalidPresidentialPardonForm( void ) {
 		try {
+			PresidentialPardonForm ppf1("Target1");
 			Bureaucrat b1("Bureaucrat1", 10);
-			Form f1("Form1", 10, 10);
-			b1.decrementGrade();
-			b1.signForm(f1);
-			std::cout << f1 << std::endl;
+			ppf1.beSigned(b1);
+			b1.executeForm(ppf1);
+			std::cout << ppf1 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
+
 		try {
-			Bureaucrat b2("Bureaucrat2", 120);
-			Form f2("Form2", 100, 100);
-			b2.signForm(f2);
-			std::cout << f2 << std::endl;
+			PresidentialPardonForm ppf2("Target1");
+			Bureaucrat b1("Bureaucrat1", 26);
+			ppf2.beSigned(b1);
+			b1.executeForm(ppf2);
+			std::cout << ppf2 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+
+		try {
+			PresidentialPardonForm ppf3("Target1");
+			Bureaucrat b1("Bureaucrat1", 5);
+			b1.executeForm(ppf3);
+			std::cout << ppf3 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
@@ -87,28 +68,16 @@ namespace Tests {
 }
 
 int main( void ) {
-	Tests::repeatChar('=', 44, CYAN);
-	std::cout << "              Valid Form Tests " << std::endl;
-	Tests::repeatChar('=', 44, CYAN);
-	Tests::testValidForm();
+	Tests::repeatChar('=', 42, CYAN);
+	std::cout << "      Valid Presidential Pardon Form " << std::endl;
+	Tests::repeatChar('=', 42, CYAN);
+	Tests::testValidPresidentialPardonForm();
 
 	std::cout << std::endl;
-	Tests::repeatChar('=', 44, CYAN);
-	std::cout << "             Invalid Form Tests " << std::endl;
-	Tests::repeatChar('=', 44, CYAN);
-	Tests::testInvalidForm();
-
-	std::cout << std::endl;
-	Tests::repeatChar('=', 44, CYAN);
-	std::cout << "            Valid Sign Form Tests " << std::endl;
-	Tests::repeatChar('=', 44, CYAN);
-	Tests::testValidSignForm();
-
-	std::cout << std::endl;
-	Tests::repeatChar('=', 44, CYAN);
-	std::cout << "           Invalid Sign Form Tests " << std::endl;
-	Tests::repeatChar('=', 44, CYAN);
-	Tests::testInvalidSignForm();
+	Tests::repeatChar('=', 42, CYAN);
+	std::cout << "    Invalid Presidential Pardon Form " << std::endl;
+	Tests::repeatChar('=', 42, CYAN);
+	Tests::testInvalidPresidentialPardonForm();
 
 	return (0);
 }
