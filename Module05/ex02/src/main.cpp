8c31d4e759e4207797474fb96c7a78c56629d432
Arthur <tutusobreira@gmail.com>
2024-06-30 22:18:12 -0300

    Update main.cpp

--- Module05/ex02/src/main.cpp
+++ Module05/ex02/src/main.cpp
@@ -2,6 +2,7 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 namespace Tests {
 	void	repeatChar( char c, int n, std::string color ) {
@@ -45,22 +46,73 @@ namespace Tests {
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
-
 		try {
 			PresidentialPardonForm ppf2("Target1");
-			Bureaucrat b1("Bureaucrat1", 26);
-			ppf2.beSigned(b1);
-			b1.executeForm(ppf2);
+			Bureaucrat b2("Bureaucrat1", 26);
+			ppf2.beSigned(b2);
+			b2.executeForm(ppf2);
 			std::cout << ppf2 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
-
 		try {
 			PresidentialPardonForm ppf3("Target1");
-			Bureaucrat b1("Bureaucrat1", 5);
-			b1.executeForm(ppf3);
+			Bureaucrat b3("Bureaucrat1", 5);
+			b3.executeForm(ppf3);
 			std::cout << ppf3 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+	}
+
+	void	testValidRobotomyRequestForm( void ) {
+		try {
+			RobotomyRequestForm rrf1;
+			std::cout << rrf1 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+		try {
+			RobotomyRequestForm rrf2("Target1");
+			std::cout << rrf2 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+		try {
+			RobotomyRequestForm rrf3("Target2");
+			Bureaucrat b1("Bureaucrat1", 5);
+			rrf3.beSigned(b1);
+			b1.executeForm(rrf3);
+			std::cout << rrf3 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+	}
+
+	void	testInvalidRobotomyRequestForm( void ) {
+		try {
+			RobotomyRequestForm rrf1("Target1");
+			Bureaucrat b1("Bureaucrat1", 50);
+			rrf1.beSigned(b1);
+			b1.executeForm(rrf1);
+			std::cout << rrf1 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+		try {
+			RobotomyRequestForm rrf2("Target1");
+			Bureaucrat b2("Bureaucrat1", 75);
+			rrf2.beSigned(b2);
+			b2.executeForm(rrf2);
+			std::cout << rrf2 << std::endl;
+		} catch (const std::exception &e) {
+			std::cout << COLORIZE(RED, e.what()) << std::endl;
+		}
+		try {
+			RobotomyRequestForm rrf3("Target1");
+			Bureaucrat b3("Bureaucrat1", 40);
+			b3.executeForm(rrf3);
+			std::cout << rrf3 << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << COLORIZE(RED, e.what()) << std::endl;
 		}
@@ -75,9 +127,21 @@ int main( void ) {
 
 	std::cout << std::endl;
 	Tests::repeatChar('=', 42, CYAN);
-	std::cout << "    Invalid Presidential Pardon Form " << std::endl;
+	std::cout << "     Invalid Presidential Pardon Form " << std::endl;
 	Tests::repeatChar('=', 42, CYAN);
 	Tests::testInvalidPresidentialPardonForm();
+
+	std::cout << std::endl;
+	Tests::repeatChar('=', 42, CYAN);
+	std::cout << "        Valid Robotomy Request Form " << std::endl;
+	Tests::repeatChar('=', 42, CYAN);
+	Tests::testValidRobotomyRequestForm();
+
+	std::cout << std::endl;
+	Tests::repeatChar('=', 42, CYAN);
+	std::cout << "       Invalid Robotomy Request Form " << std::endl;
+	Tests::repeatChar('=', 42, CYAN);
+	Tests::testInvalidRobotomyRequestForm();
 
 	return (0);
 }
