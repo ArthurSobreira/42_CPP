5cc0103e306672a69041a6b19ef50b515fd2410b
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-06 01:04:25 -0300

    Update Data.cpp

---
+++ Module06/ex01/src/Data.cpp
@@ -0,0 +1,25 @@
+#include "Data.hpp"
+
+/* Constructor Method */
+Data::Data( void ) : _attribute(10) {};
+
+/* Copy Constructor Method */
+Data::Data( const Data &other ) {
+	*this = other;
+}
+
+/* Copy Assignment Operator Overload */
+Data &Data::operator=( const Data &other ) {
+	if ( this != &other ) {
+		this->attribute = other.getAttribute();
+	}
+	return (*this);
+}
+
+/* Destructor Method */
+Data::~Data( void ) {}
+
+ /* Public Methods */
+int Data::getAttribute( void ) const {
+	return (this->attribute);
+}
