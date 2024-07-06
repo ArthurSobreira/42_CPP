2d5d34886680c5a9bf23db459841ffff4ca4a078
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-06 01:00:31 -0300

    Update Data.hpp

---
+++ Module06/ex01/includes/Data.hpp
@@ -0,0 +1,25 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+class Data {
+	private:
+		int	_attribute;
+		
+	public:
+		/* Constructor Method */
+		Data( void );
+		
+		/* Copy Constructor Method */
+		Data( const Data &other );
+		
+		/* Copy Assignment Operator Overload */
+		Data &operator=( const Data &other );
+		
+		/* Destructor Method */
+		~Data( void );
+		
+		/* Public Methods */
+		int    getAttribute( void ) const;
+};
+
+#endif
