0d614fe1401f74cf2cdc57f7fef06b7d892521a1
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-06 04:51:15 -0300

    Update main.cpp

---
+++ Module06/ex02/src/main.cpp
@@ -0,0 +1,37 @@
+#include "Defines.hpp"
+#include "Base.hpp"
+#include <iostream>
+
+namespace Tests {
+	void	repeatChar( char c, int n, std::string color ) {
+		for (int i = 0; i < n; i++) {
+			std::cout << color << c;
+		}
+		std::cout << RESET << std::endl;
+	}
+
+	void	testIdentifyFromPointerAndReference( void ) {
+		for (size_t i = 0; i < 3; i++)
+		{
+			Base	*base = generate();
+
+			std::cout << COLORIZE(YELLOW, "Identify Base from Pointer:   ");
+			identify(base);
+			std::cout << COLORIZE(YELLOW, "Identify Base from Reference: ");
+			identify(*base);
+			std::cout << std::endl;
+
+			delete base;
+		}
+	}
+
+}
+
+int main( void ) {
+	Tests::repeatChar('=', 41, CYAN);
+	std::cout << "     Test Identify Base from Pointer and Reference " << std::endl;
+	Tests::repeatChar('=', 41, CYAN);
+	Tests::testIdentifyFromPointerAndReference();
+
+	return (0);
+}
