5a397b5eec638e7e86576988927a601c1092ce7c
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-09 18:00:11 -0300

    Update Array.hpp

---
+++ Module07/ex02/includes/Array.hpp
@@ -0,0 +1,32 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+typedef unsigned int	uint;
+
+template <typename T> class Array {
+	private:
+		T		*_array;
+		uint	_size;
+		
+	public:
+		/* Constructor Method */
+		Array( void );
+		Array( uint n )
+		
+		/* Copy Constructor Method */
+		Array( const Array &other );
+		
+		/* Copy Assignment Operator Overload */
+		Array &operator=( const Array &other );
+		
+		/* Destructor Method */
+		~Array( void );
+		
+		/* Public Methods */
+		unsigned int	size( void ) const;
+		T &operator[]( uint index );
+};
+
+#include "../src/Array.tpp"
+
+#endif
