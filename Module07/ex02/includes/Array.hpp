da79fa80720c25b4acc0225f70cb983a603da394
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-09 19:12:34 -0300

    Update Array.hpp

--- Module07/ex02/includes/Array.hpp
+++ Module07/ex02/includes/Array.hpp
@@ -3,13 +3,16 @@
 
 typedef unsigned int	uint;
 
+#include <iostream>
+#include <stdexcept>
+
 template <typename T> class Array {
 	private:
 		T		*_array;
 		uint	_size;
 		
 	public:
-		/* Constructor Method */
+		/* Constructor Methods */
 		Array( void );
 		Array( uint n )
 		
@@ -25,6 +28,17 @@ template <typename T> class Array {
 		/* Public Methods */
 		uint	size( void ) const;
 		T &operator[]( uint index );
+
+		/* Exception Classes */
+		class OutOfLimitsException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+
+		class EmptyArrayException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
 };
 
 #include "../src/Array.tpp"
