703cd96d3246252f0f5d5f57bde27b40e5136d85
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-09 15:20:34 -0300

    Update main.cpp

---
+++ Module07/ex01/src/main.cpp
@@ -0,0 +1,65 @@
+#include "Defines.hpp"
+#include "iter.hpp"
+#include <string>
+
+namespace Tests {
+	void	repeatChar( char c, int n, std::string color ) {
+		for (int i = 0; i < n; i++) {
+			std::cout << color << c;
+		}
+		std::cout << RESET << std::endl;
+	}
+
+	void	invertCase( std::string const &str ) {
+		std::string &copy = const_cast<std::string &>(str);
+		for (size_t i = 0; i < copy.length(); i++) {
+			if (copy[i] >= 'A' && copy[i] <= 'Z') {
+				copy[i] += 32;
+			} else if (copy[i] >= 'a' && copy[i] <= 'z') {
+				copy[i] -= 32;
+			}
+		}
+	}
+
+	void	isEven( int const &n ) {
+		std::cout << n << " is ";
+		if (n % 2 == 0) {
+			std::cout << COLORIZE(GREEN, "even");
+		} else {
+			std::cout << COLORIZE(RED, "odd");
+		}
+		std::cout << std::endl;
+	}
+
+	void	testStringArray( void ) {
+		std::string strings[] = {
+			"Hello",
+			"World",
+			"42",
+			"Cpp",
+			"Module07"
+		};
+		iter( strings, 5, invertCase );
+		iter( strings, 5, print );
+	}
+
+	void	testIntArray( void ) {
+		int ints[] = { 1, 2, 3, 4, 5 };
+
+		iter( ints, 5, isEven );
+	}
+}
+
+int		main( void ) {
+	Tests::repeatChar( '=', 36, BOLD_CYAN );
+	std::cout << "         String Array Tests " << std::endl;
+	Tests::repeatChar( '=', 36, BOLD_CYAN );
+	Tests::testStringArray();
+
+	Tests::repeatChar( '=', 36, BOLD_CYAN );
+	std::cout << "         Integer Array Tests " << std::endl;
+	Tests::repeatChar( '=', 36, BOLD_CYAN );
+	Tests::testIntArray();
+	
+	return (0);
+}
