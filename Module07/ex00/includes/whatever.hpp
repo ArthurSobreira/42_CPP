6a65ce4a0884d466e0b93bb71ed0b3faa7a67b9e
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-09 13:44:42 -0300

    Update whatever.hpp

--- Module07/ex00/includes/whatever.hpp
+++ Module07/ex00/includes/whatever.hpp
@@ -1,12 +1,14 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
+/* Template Swap Function */
 template <typename T> void	swap( T &a, T &b ) {
 	T temp = a;
 	a = b;
 	b = temp;
 }
 
+/* Template Min and Max Functions */
 template <typename T> T min( T const &a, T const &b ) {
 	return ( a < b ? a : b );
 }
