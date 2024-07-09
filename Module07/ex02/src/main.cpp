dc4abc85186848a5f9566e927345f5325186bf84
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-09 20:24:24 -0300

    Update main.cpp

--- Module07/ex02/src/main.cpp
+++ Module07/ex02/src/main.cpp
@@ -4,29 +4,28 @@
 #include <ctime>
 
 namespace Utils {
-    void    fillIntArray( Array<int> &array ) {
-        std::srand(std::time(NULL));
-        for (uint i = 0; i < array.size(); i++) {
-            array[i] = rand() % 100;
-        }
-    }
+	void	fillIntArray( Array<int> &array ) {
+		std::srand(std::time(NULL));
+		for (uint i = 0; i < array.size(); i++) {
+			array[i] = rand() % 100;
+		}
+	}
 
-    void    fillStringArray( Array<std::string> &array ) {
-        std::string strings[] = {
+	void	fillStringArray( Array<std::string> &array ) {
+		std::string strings[] = {
 			"Hello", "World", "42", "Cpp", "Module07"
 		};
-        for (uint i = 0; i < array.size(); i++) {
-            array[i] = strings[rand() % 5];
-        }
-    }
+		for (uint i = 0; i < array.size(); i++) {
+			array[i] = strings[rand() % 5];
+		}
+	}
 
-    template <typename T>
-    void    printArray( Array<T> &array ) {
-        for (uint i = 0; i < array.size(); i++) {
-            std::cout << CYAN "Array[" << i << "]: " RESET << 
-            array[i] << std::endl;
-        }
-    }
+	template <typename T> void	printArray( Array<T> &array ) {
+		for (uint i = 0; i < array.size(); i++) {
+			std::cout << CYAN "Array[" << i << "]: " RESET << 
+			array[i] << std::endl;
+		}
+	}
 }
 
 namespace Tests {
@@ -37,24 +36,24 @@ namespace Tests {
 		std::cout << RESET << std::endl;
 	}
 
-    void    testValidArray( void ) {
-        Array<int> array1(5);
+	void	testValidArray( void ) {
+		Array<int> array1(5);
 
-        std::cout << COLORIZE( BLUE, "Array1 size: " ) << 
-        array1.size() << std::endl;
-        Utils::fillIntArray(array1);
-        Utils::printArray(array1);
+		std::cout << COLORIZE( BLUE, "Array1 size: " ) << 
+		array1.size() << std::endl;
+		Utils::fillIntArray(array1);
+		Utils::printArray(array1);
 
-        Array<std::string> array2(5);
+		Array<std::string> array2(5);
 
-        std::cout << COLORIZE( BLUE, "Array2 size: " ) <<
-        array2.size() << std::endl;
-        Utils::fillStringArray(array2);
-        Utils::printArray(array2);
-    }
+		std::cout << COLORIZE( BLUE, "Array2 size: " ) <<
+		array2.size() << std::endl;
+		Utils::fillStringArray(array2);
+		Utils::printArray(array2);
+	}
 
-    void    testInvalidArray( void ) {
-        Array<int> array1(2);
+	void	testInvalidArray( void ) {
+		Array<int> array1(2);
 		Array<std::string> array2;
 
 		try {
@@ -68,19 +67,19 @@ namespace Tests {
 		} catch (std::exception& e) {
 			std::cout << e.what() << std::endl;
 		}
-    }
+	}
 }
 
 int main( void ){
-    Tests::repeatChar( '=', 36, BOLD_CYAN );
-    std::cout << "         Valid Array Tests " << std::endl;
-    Tests::repeatChar( '=', 36, BOLD_CYAN );
-    Tests::testValidArray();
+	Tests::repeatChar( '=', 36, BOLD_CYAN );
+	std::cout << "         Valid Array Tests " << std::endl;
+	Tests::repeatChar( '=', 36, BOLD_CYAN );
+	Tests::testValidArray();
 
-    Tests::repeatChar( '=', 36, BOLD_CYAN );
-    std::cout << "        Invalid Array Tests " << std::endl;
-    Tests::repeatChar( '=', 36, BOLD_CYAN );
-    Tests::testInvalidArray();
+	Tests::repeatChar( '=', 36, BOLD_CYAN );
+	std::cout << "        Invalid Array Tests " << std::endl;
+	Tests::repeatChar( '=', 36, BOLD_CYAN );
+	Tests::testInvalidArray();
 
-    return (0);
+	return (0);
 }
