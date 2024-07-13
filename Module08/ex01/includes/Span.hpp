fa2f3de267054c639eced4087fda6dbff80255b1
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-13 03:42:28 -0300

    Update Span.hpp

---
+++ Module08/ex01/includes/Span.hpp
@@ -0,0 +1,49 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+typedef unsigned int	uint;
+
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+class Span {
+	private:
+		std::vector<int>	_list;
+		const uint	_size;
+		
+	public:
+		/* Constructor Methods */
+		Span( void );
+		Span( uint size );
+		
+		/* Copy Constructor Method */
+		Span( const Span &other );
+		
+		/* Copy Assignment Operator Overload */
+		Span &operator=( const Span &other );
+		
+		/* Destructor Method */
+		~Span( void );
+		
+		/* Public Methods */
+		void	addNumber( int number );
+		int		shortestSpan( void );
+		int		longestSpan( void );
+		template <typename Iterator> 
+		void	populate( Iterator begin, Iterator end );
+
+		/* Exception Classes */
+		class LimitReachedException : public std::exception {
+			public:
+				virtual const char *what( void ) const throw();
+		};
+
+		class NotEnoughNumbersException : public std::exception {
+			public:
+				virtual const char *what( void ) const throw();
+		};
+};
+
+#endif
