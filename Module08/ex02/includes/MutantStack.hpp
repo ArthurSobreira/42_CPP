ec882d1927ff5e7937b971afdac2b8f2ebb832e6
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-16 17:00:24 -0300

    Update MutantStack.hpp

---
+++ Module08/ex02/includes/MutantStack.hpp
@@ -0,0 +1,36 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <vector>
+
+template <typename T>
+class MutantStack : public std::stack<T> {	
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		/* Constructor Method */
+		MutantStack( void );
+		
+		/* Copy Constructor Method */
+		MutantStack( const MutantStack &other );
+		
+		/* Copy Assignment Operator Overload */
+		MutantStack<T> &operator=( const MutantStack &other );
+		
+		/* Destructor Method */
+		~MutantStack( void );
+		
+		/* Public Methods */
+		iterator begin( void );
+		iterator end( void );
+		const_iterator begin( void ) const;
+		const_iterator end( void ) const;
+};
+
+#include "../src/MutantStack.tpp"
+
+#endif
