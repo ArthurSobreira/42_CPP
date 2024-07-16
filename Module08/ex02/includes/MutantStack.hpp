2f861e309fc18ecee096aeef09c0c384eb1a61eb
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-16 17:18:15 -0300

    Update MutantStack.hpp

--- Module08/ex02/includes/MutantStack.hpp
+++ Module08/ex02/includes/MutantStack.hpp
@@ -9,6 +9,7 @@
 template <typename T>
 class MutantStack : public std::stack<T> {	
 	public:
+		/* Iterator Typedefs */
 		typedef typename std::stack<T>::container_type::iterator iterator;
 		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
