fe44f135127a1d5fd64f2437e65bb7cad6e93de6
Arthur Sobreira <tutusobreira@gmail.com>
2024-07-19 22:27:03 -0300

    Update RPN.hpp

---
+++ Module09/ex01/includes/RPN.hpp
@@ -0,0 +1,30 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <stdexcept>
+#include <stack>
+
+class RPN {
+	private:
+		std::string	_expression;
+
+	public:
+		/* Constructor Method */
+		RPN( void );
+		RPN( std::string expression );
+
+		/* Copy Constructor Method */
+		RPN( const RPN &other );
+
+		/* Copy Assignment Operator Overload */
+		RPN &operator=( const RPN &other );
+		
+		/* Destructor Method */
+		~RPN( void );
+		
+		/* Public Methods */
+		size_t	calculate( void ) const;
+};
+
+#endif
